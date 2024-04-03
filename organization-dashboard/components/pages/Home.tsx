c7c63c58a26dc9b25026bde3796512794098dcf7
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-04-03 16:07:28 +0000

    updated

--- organization-dashboard/components/pages/Home.tsx
+++ organization-dashboard/components/pages/Home.tsx
@@ -1,18 +1,20 @@
 import Image from 'next/image';
 import { Wallet, Card } from '../shared/shared';
-import {cardData} from "@/constants/cards"
+import { cardData } from '@/constants/cards';
 
 export default function HomePage() {
-    return (
-      <main className="bg-[#E1D7E2] mt-[70px]">
-        <div className="flex items-center justify-center gap-6"> 
-          <div className="mr-auto w-[40%]"> 
-            <p className="font-bold text-[28px] text-[#38313A]"> Let's help you get started today! </p>
-          </div>
-          <Wallet />
+  return (
+    <main className="bg-[#E1D7E2] mt-[70px]">
+      <div className="flex items-center justify-center gap-6">
+        <div className="mr-auto w-[40%]">
+          <p className="font-bold text-[28px] text-[#38313A]">
+            Let&apos;s help you get started today!
+          </p>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 justify-center mt-[40px]">
-          {cardData.map((card, index) => (
+        <Wallet />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 justify-center mt-[40px]">
+        {cardData.map((card, index) => (
           <Card
             key={index} // Ensure each Card component has a unique key
             heading={card.heading}
@@ -20,8 +22,8 @@ export default function HomePage() {
             bgColor={card.bgColor}
             outlineColor={card.outlineColor}
           />
-          ))}
-        </div>
-      </main>
-    )
+        ))}
+      </div>
+    </main>
+  );
 }
